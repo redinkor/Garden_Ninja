cc96c420b3510c375029dbc1e43dcaa776e1c4d0
kynikov <hellboynikov@gmail.com>
2022-09-01 22:04:46 +0300

    Alex__Konstantinov/garden-ninja/contact.html

--- Alex__Konstantinov/garden-ninja/JS/script.js
+++ Alex__Konstantinov/garden-ninja/JS/script.js
@@ -1,63 +1,4 @@
-"use strict"
-
-document.addEventListener('DOMContentLoaded', function (){
-  const form = document.getElementById('form');
-  form.getEventListener('submit', formSend);
-
-  async function formSend(e) {
-    e.preventDefault();
-
-    let error = formValidate(form);
-    
-    if (error === 0) {
-
-    } else  {
-      alert('Заполните обезательные поля');
-    }
-
-  }
-
-  function formValidate(form) {
-    let error = 0;
-    let formReq = document.querySelectorAll('._req');
-
-    for(let index = 0; index < formReq.length; index++) {
-      const input = formReq[index];
-      formRemoveError(input);
-
-      if(input.classList.contains('_email')) {
-        if (emailTest(input)) {
-          formAddError(input);
-          error++;
-        }       
-      } else if (input.getAttribute('type') === "checkbox" && input.checked === false) {
-        formAddError(input);
-        error++;
-      } else {
-        if (input.value === '') {
-          formAddError(input);
-          error++;
-        }
-      }
-
-    }
-  }
-  function formAddError(input) {
-    input.parentElement.classList.add('_error');
-    input.classList.add('_error');
-  }
-  function formRemoveError(input) {
-    input.parentElement.classList.add('_error');
-    input.classList.remove('_error');
-  }
-  //function test email
-  function emailTest(input) {
-    return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-  }
-});
-
-/////////////////////////////////////////////////////////////////
-
+/* --------------------------------form design------------------------------- */
 const inputs = document.querySelectorAll(".input");
 
 function focusFunc() {
@@ -76,48 +17,7 @@ inputs.forEach((input) => {
   input.addEventListener("focus", focusFunc);
   input.addEventListener("blur", blurFunc);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/* ----------------------------slider----------------------- */
 $(function() {
 
   $('.slider__inner, .news__slider-inner')
